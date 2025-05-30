3bd9a90a70f1d00c327859afbb5a7336ffa14671
Mit <mitmirani454@gmail.com>
2025-05-30 17:39:48 +0530

    re install tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = {
   ...compat.config({
-    extends:["next/core-web-vitals", "next/typescript", 'prettier', 'plugin:tailwindcss/recommended'],
+    extends:["next/core-web-vitals", "next/typescript", 'prettier'],
     plugins:['prettier'],
     rules:{
       'prettier/prettier': 'error',
